5d3755e20b5e039b0b617febf8c1297c591a0770
Aurelius Wendelken <webtobesocial@gmail.com>
2025-02-25 14:25:40 +0100

    add filter to endpoint space

--- app/api/v1/endpoints/space.py
+++ app/api/v1/endpoints/space.py
@@ -1,16 +1,44 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_db
 from app.schemas.space import Space
-from app.db.repository.space import get_all_spaces
-from typing import List
+from app.db.repository.space import get_all_spaces, get_spaces_by_filter
+from typing import List, Optional
 
 
 
 router = APIRouter()
 
 @router.get('/', response_model=List[Space])
-async def fetch_all_spaces(db: AsyncSession = Depends(get_db)):
+async def fetch_all_spaces(
+    db: AsyncSession = Depends(get_db)
+):
     spaces = await get_all_spaces(db)
+
+    return spaces
+
+
+
+@router.get('/filtered', response_model=List[Space])
+async def fetch_spaces_by_filter(
+    space_id: Optional[int] = Query(None),
+    venue_id: Optional[int] = Query(None),
+    space_type_id: Optional[int] = Query(None),
+    db: AsyncSession = Depends(get_db)
+):
+    filters = {
+        'id': space_id,
+        'venue_id': venue_id,
+        'space_type_id': space_type_id
+    }
+
+    # Remove filters with None values
+    active_filters = {key: value for key, value in filters.items() if value not in [None, '']}
+
+    if not active_filters:
+        raise HTTPException(status_code=400, detail='At least one filter parameter is required')
+
+
+    spaces = await get_spaces_by_filter(db, active_filters)
 
     return spaces
--- app/db/repository/space.py
+++ app/db/repository/space.py
@@ -9,3 +9,19 @@ async def get_all_spaces(db: AsyncSession):
     
     spaces = result.scalars().all()
     return spaces
+
+
+
+async def get_spaces_by_filter(db: AsyncSession, filters: dict):
+    stmt = (select(Space))
+
+    # Apply multiple filters dynamically
+    for column_name, filter_value in filters.items():
+        column = getattr(Space, column_name)
+
+        stmt = stmt.where(column == filter_value)
+
+    result = await db.execute(stmt)
+    spaces = result.scalars().all()
+
+    return spaces
