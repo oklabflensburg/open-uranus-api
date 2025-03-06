371acc5b353fd7575f94cbb96d3dfd83afafdd73
Aurelius Wendelken <webtobesocial@gmail.com>
2025-03-06 15:58:11 +0100

    remove prettier in favour for js-beautify

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  semi: false,
-  trailingComma: 'none',
-  bracketSameLine: true,
-  endOfLine: 'auto',
-  printWidth: 999,
-  htmlWhitespaceSensitivity: "ignore"
-}
